de44568fdef41ed3d5f72975fe7a839d16facdbe
zhengshijun <zsjdxc251@sina.com>
2019-03-04 22:40:25 +0800

    分布式 事务

---
+++ example-alibaba-fescar/example-alibaba-fescar-quickstart/src/main/java/com/lesson/alibaba/fescar/quickstart/FescarQuickstartBootstrap.java
@@ -0,0 +1,15 @@
+package com.lesson.alibaba.fescar.quickstart;
+
+import javax.transaction.xa.XAResource;
+
+/**
+ * @author zhengshijun
+ * @version created on 2019/3/4.
+ */
+public class FescarQuickstartBootstrap {
+
+    public static void main(String[] args){
+
+        XAResource xaResource;
+    }
+}
